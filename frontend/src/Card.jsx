aae73ae8e791b4b5c42ddc4c8e97ed713c430725
Harshit kumar <harshitkumar30012006@gmail.com>
2025-11-14 22:38:30 +0530

    Some frontend improvement

--- frontend/src/Card.jsx
+++ frontend/src/Card.jsx
@@ -1,15 +1,26 @@
 
 
 const Card = ({url,text,buttontext}) => {
+    if (buttontext){return(
+        <>
+            <div className="card">
+                <img src={url} alt="img" className="card-img"></img>
+                <p className="card-content">{text}</p>
+                <button className="buttons">{buttontext}</button>
+            </div>
+        </>
+    )
+}
+else{
     return(
         <>
             <div className="card">
                 <img src={url} alt="img" className="card-img"></img>
                 <p className="card-content">{text}</p>
-                <button>{buttontext}</button>
             </div>
         </>
     )
+}
 }
 
 export default Card
